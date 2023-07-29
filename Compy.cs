c444d812f6b2629b1a6d06e2aece6057133f4ca3
James Fitt <91226933+Dragonshifty@users.noreply.github.com>
2023-07-29 14:27:11 +0100

    Add files via upload

--- Compy.cs
+++ Compy.cs
@@ -7,14 +7,17 @@ public class Compy : MonoBehaviour
     public GameObject CompyChoice(List<GameObject> compyHand, GameObject cardInPlay, int deckCount)
     {
         int[] scoreTemp = new int[5];
+        int[] baseValues = new int[5];
         int cardInPlayValue = cardInPlay.GetComponent<ObjectDetails>().CardValue;
         string cardInPlayHouse = cardInPlay.GetComponent<ObjectDetails>().House;
+        
 
         for (int i = 0; i < compyHand.Count; i++)
         {
             int cardValue = compyHand[i].GetComponent<ObjectDetails>().CardValue;
             int highLowDraw = cardValue.CompareTo(cardInPlayValue);
             bool isAMatch = compyHand[i].GetComponent<ObjectDetails>().House.Equals(cardInPlayHouse);
+            baseValues[i] = cardValue;
             int difference = 0;
             switch (highLowDraw)
                 {
@@ -32,12 +35,12 @@ public class Compy : MonoBehaviour
                 }
         }
 
-        int returnIndex = RunPossibleChoice(scoreTemp, deckCount);
+        int returnIndex = RunPossibleChoice(scoreTemp, deckCount, baseValues);
 
         return compyHand[returnIndex];
     }
 
-    int RunPossibleChoice(int[] scoreTemp, int deckCount)
+    int RunPossibleChoice(int[] scoreTemp, int deckCount, int[] baseValues)
     {
         int max = scoreTemp[0];
         int secondHighest = int.MinValue;
@@ -63,6 +66,33 @@ public class Compy : MonoBehaviour
         {
             int rand = UnityEngine.Random.Range(0, 2);
             if (rand == 0) returnIndex = returnIndex2;
+        }
+
+        if (deckCount > 28 && max < 6)
+        {
+            int rand = UnityEngine.Random.Range(0, 3);
+            if (rand == 0) returnIndex = SaveHigherCard(returnIndex, scoreTemp, baseValues);
+        }
+       
+        return returnIndex;
+    }
+
+    int SaveHigherCard(int returnIndex, int[] scoreTemp, int[] baseValues)
+    {
+        if (baseValues[4] > 9)
+        {
+            for (int i = 3; i >= 0; i--)
+            {
+                if (scoreTemp[i] > 0) returnIndex = i;
+            }
+        }
+
+        if (baseValues[3] > 9)
+        {
+            for (int i = 2; i >= 0; i--)
+            {
+                if (scoreTemp[i] > 0) returnIndex = i;
+            }
         }
         return returnIndex;
     }
